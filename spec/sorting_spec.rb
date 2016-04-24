68a4f32c3740610ae20412b5b64f8e4200dc92e6
Ben <ben.c.hawker@gmail.com>
2016-04-24 14:41:24 +0800

    Merge sort

--- spec/sorting_spec.rb
+++ spec/sorting_spec.rb
@@ -26,4 +26,28 @@ describe "Bubble Sort" do
     end
   end
 
+   describe "#merge_sort" do
+    it 'sorts correctly' do
+      array = [1,5,5,6,7,8,2,3,5,7,8,7,432,4,6,6,2,3,5,6,3]
+      expect(merge_sort(array)).to eq array.sort
+    end
+
+    it 'sorts correctly' do
+      array = [1,5,23445,6,7,83245364,2,3,5,4536457,8,7,432,4,3454656,6,2,332435,525434,6,3]
+      expect(merge_sort(array)).to eq array.sort
+    end
+  end
+
+     describe "#merge_sort_two" do
+    it 'sorts correctly' do
+      array = [1,5,5,6,7,8,2,3,5,7,8,7,432,4,6,6,2,3,5,6,3]
+      expect(merge_sort_two(array)).to eq array.sort
+    end
+
+    it 'sorts correctly' do
+      array = [1,5,23445,6,7,83245364,2,3,5,4536457,8,7,432,4,3454656,6,2,332435,525434,6,3]
+      expect(merge_sort_two(array)).to eq array.sort
+    end
+  end
+
 end
